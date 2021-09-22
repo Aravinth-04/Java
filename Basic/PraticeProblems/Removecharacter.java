c4e17714c3744d01ff7528ff72191ce72e72f258
Aravinth <74677913+Aravinth-04@users.noreply.github.com>
2021-09-22 17:00:34 +0530

    Create Removecharacter.java

---
+++ Basic/PraticeProblems/Removecharacter.java
@@ -0,0 +1,21 @@
+
+Input:
+string1 = "computer"
+string2 = "cat"
+Output: "ompuer"
+Explanation: After removing characters(c, a, t)
+from string1 we get "ompuer".
+
+
+public class Removecharacter
+{
+	public static void main(String[] args) {
+	String string1="computer";
+	String string2="cat";
+	System.out.println(removeChars(string1,string2));
+	}
+    static String removeChars(String string1, String string2){
+      String string3=string1.replaceAll( "["+string2+"]","");
+      return string3;
+  }
+}
